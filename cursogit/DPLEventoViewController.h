ae20f6b999d3a068b02f85cb35e58cd250b0f54e
Diego Pérez Lora <diego.perez.lora@gmail.com>
2013-03-16 15:34:24 +0100

    · Add viewController for Event

---
+++ cursogit/DPLEventoViewController.h
@@ -0,0 +1,16 @@
+//
+//  DPLEventoViewController.h
+//  cursogit
+//
+//  Created by Diego Pérez Lora on 16/03/13.
+//  Copyright (c) 2013 Diego Pérez Lora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "DPLEvento.h"
+
+@interface DPLEventoViewController : UIViewController
+
+@property (nonatomic, strong) DPLEvento *evento;
+
+@end
---
+++ cursogit/DPLEventoViewController.m
@@ -0,0 +1,38 @@
+//
+//  DPLEventoViewController.m
+//  cursogit
+//
+//  Created by Diego Pérez Lora on 16/03/13.
+//  Copyright (c) 2013 Diego Pérez Lora. All rights reserved.
+//
+
+#import "DPLEventoViewController.h"
+
+@interface DPLEventoViewController ()
+
+@end
+
+@implementation DPLEventoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

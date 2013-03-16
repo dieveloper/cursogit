de15a63610eebb8721875aaaed52c87245161545
Diego Pérez Lora <diego.perez.lora@gmail.com>
2013-03-16 12:04:12 +0100

    · Add empty model class Alumno.

---
+++ cursogit/DPLAlumno.h
@@ -0,0 +1,13 @@
+//
+//  DPLAlumno.h
+//  cursogit
+//
+//  Created by Diego Pérez Lora on 16/03/13.
+//  Copyright (c) 2013 Diego Pérez Lora. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DPLAlumno : NSObject
+
+@end
---
+++ cursogit/DPLAlumno.m
@@ -0,0 +1,13 @@
+//
+//  DPLAlumno.m
+//  cursogit
+//
+//  Created by Diego Pérez Lora on 16/03/13.
+//  Copyright (c) 2013 Diego Pérez Lora. All rights reserved.
+//
+
+#import "DPLAlumno.h"
+
+@implementation DPLAlumno
+
+@end
